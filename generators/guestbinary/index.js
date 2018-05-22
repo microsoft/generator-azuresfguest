03f8f1be73ad07f3d4d661215c8c1bd5e7ae0c3c
Arram Saichand <t-arsaic@microsoft.com>
2018-05-22 16:33:18 +0530

    Addressed Review comments

--- generators/guestbinary/index.js
+++ generators/guestbinary/index.js
@@ -149,19 +149,16 @@ module.exports = generators.Base.extend({
             var serviceTypeName = this.props.serviceName + 'Type';
             var appTypeName = this.projName + 'Type';
             var pkgDir = this.isAddNewService == false ? path.join(this.projName, this.projName) : this.projName;
-            var is_Windows = (process.platform=='win32');
-            var is_Linux = (process.platform=='linux');
-            var is_mac = (process.platform=='darwin');
-            var extension1;
-            var extension2;
+            var is_Windows = (process.platform == 'win32');
+            var is_Linux = (process.platform == 'linux');
+            var is_mac = (process.platform == 'darwin');
+            var sdkScriptExtension;
             if(is_Windows)
             {
-                extension1 = '.ps1';
-                extension2 = '.cmd';
+                sdkScriptExtension = '.ps1';
             }
-            else if(is_Linux){
-                extension1 = '.sh';
-                extension2 = '.sh';
+            else {
+                sdkScriptExtension = '.sh';
             }
 
             this.fs.copyTpl(  this.templatePath('Service/ServiceManifest.xml'),
@@ -183,8 +180,8 @@ module.exports = generators.Base.extend({
             );
             if (!this.isAddNewService) {
                 this.fs.copyTpl(
-                    this.templatePath('deploy/install'+extension1),
-                    this.destinationPath(path.join(this.projName, 'install'+extension1)),
+                    this.templatePath('deploy/install'+sdkScriptExtension),
+                    this.destinationPath(path.join(this.projName, 'install'+sdkScriptExtension)),
                     {
                         appPackage: this.projName,
                         appName: this.projName,
@@ -193,19 +190,12 @@ module.exports = generators.Base.extend({
                 );
 
                 this.fs.copyTpl(
-                    this.templatePath('deploy/uninstall'+extension1),
-                    this.destinationPath(path.join(this.projName, 'uninstall'+extension1)),
+                    this.templatePath('deploy/uninstall'+sdkScriptExtension),
+                    this.destinationPath(path.join(this.projName, 'uninstall'+sdkScriptExtension)),
                     {
                         appPackage: this.projName,
                         appName: this.projName,
                         appTypeName: appTypeName
-                    }
-                );
-
-                this.fs.copyTpl(
-                    this.templatePath('deploy/preinstall'+extension1),
-                    this.destinationPath(path.join(this.projName, 'preinstall'+extension1)),
-                    {
                     }
                 );
             }
